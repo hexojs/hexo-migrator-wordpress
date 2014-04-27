e600699793c53d3d0c708ba4c9f60d2866127880
Tommy Chen <tommy351@gmail.com>
2014-04-28 00:42:06 +0800

    Refactor

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-var extend = hexo.extend,
-  util = hexo.util,
-  file = util.file,
-  sourceDir = hexo.source_dir,
-  xml2js = require('xml2js'),
-  parser = new xml2js.Parser(),
-  request = require('request'),
+var xml2js = require('xml2js'),
   async = require('async'),
-  _ = require('underscore'),
-  tomd = require('to-markdown').toMarkdown;
+  tomd = require('to-markdown').toMarkdown,
+  request = require('request'),
+  util = hexo.util,
+  file = util.file2;
 
-extend.migrator.register('wordpress', function(args){
+var captialize = function(str){
+  return str[0].toUpperCase() + str.substring(1);
+};
+
+hexo.extend.migrator.register('wordpress', function(args, callback){
   var source = args._.shift();
 
-  if (!source) return console.log('\nUsage: hexo migrate wordpress <source>\n\nMore info: http://zespia.tw/hexo/docs/migrate.html\n');
+  if (!source){
+    var help = [
+      'Usage: hexo migrate wordpress <source>',
+      '',
+      'For more help, you can check the docs: http://hexo.io/docs/migration.html'
+    ];
+
+    console.log(help.join('\n'));
+    return callback();
+  }
+
+  var log = hexo.log,
+    post = hexo.post;
+
+  log.i('Analyzing %s...', source);
 
   async.waterfall([
     function(next){
-      console.log('Fetching %s.', source);
-
       // URL regular expression from: http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
       if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/)){
         request(source, function(err, res, body){
@@ -25,99 +37,75 @@ extend.migrator.register('wordpress', function(args){
           if (res.statusCode == 200) next(null, body);
         });
       } else {
-        file.read(source, next);
+        file.readFile(source, next);
       }
     },
-    function(data, next){
-      console.log('Parsing XML.');
-      parser.parseString(data, next);
+    function(content, next){
+      xml2js.parseString(content, next);
     },
-    function(data, next){
-      console.log('Analyzing.');
+    function(xml, next){
+      var count = 0;
 
-      var length = 0,
-        arr = data.rss.channel[0].item;
+      async.each(xml.rss.channel[0].item, function(item, next){
+        if (!item['wp:post_type']){
+          return next();
+        }
 
-      async.forEach(arr, function(item, next){
-        var postTitle = item.title[0],
+        var title = item.title[0],
           id = item['wp:post_id'][0],
-          postDate = item['wp:post_date'][0],
-          postLink = item['wp:post_name'][0],
-          postContent = item['content:encoded'][0],
-          postComment = item['wp:comment_status'][0] === 'open' ? true : false;
+          date = item['wp:post_date'][0],
+          slug = item['wp:post_name'][0],
+          content = item['content:encoded'][0],
+          comment = item['wp:comment_status'][0],
+          status = item['wp:status'][0],
+          type = item['wp:post_type'][0],
+          categories = [],
+          tags = [];
 
-        if (_.isObject(postTitle)) postTitle = '';
-        if (!postLink || _.isObject(postLink)) {
-          if (postTitle)
-            postLink = postTitle.toLowerCase().split(' ').join('-');
-          else {
-            // Have to use post_id if both title and post_name are empty
-            postLink = item['wp:post_id'][0];
-          }
+        if (!title && !slug) return next();
+        if (type !== 'post' && type !== 'page') return next();
+        if (typeof content !== 'string') content = '';
+
+        content = tomd(content).replace(/\r\n/g, '\n');
+        count++;
+
+        if (item.category){
+          item.category.forEach(function(category, next){
+            var name = category._;
+
+            switch (category.$.domain){
+              case 'category':
+                categories.push(name);
+                break;
+
+              case 'post_tag':
+                tags.push(name);
+                break;
+            }
+          });
         }
 
-        postContent = _.isObject(postContent) ? '' : tomd(postContent);
-        postContent = postContent.replace(/\r\n/g, '\n');
+        var data = {
+          title: title || slug,
+          id: +id,
+          date: date,
+          content: content,
+          layout: status === 'draft' ? 'draft' : 'post',
+        };
 
-        switch (item['wp:post_type'][0]){
-          case 'post':
-            length++;
+        if (type === 'page') data.layout = 'page';
+        if (slug) data.slug = slug;
+        if (comment === 'closed') data.comment = false;
+        if (categories.length && type === 'post') data.categories = categories;
+        if (tags.length && type === 'post') data.tags = tags;
 
-            var postStatus = item['wp:status'][0] === 'publish' ? '_posts/' : '_drafts/',
-              cats = item.category,
-              categories = [],
-              postTag = [];
-
-            _.each(cats, function(item){
-              if (!_.isString(item)){
-                switch(item.$.domain){
-                  case 'post_tag':
-                    postTag.push(item._);
-                    break;
-                  case 'category':
-                    categories.push(item._);
-                    break;
-                }
-              }
-            });
-
-            if (postTag.length) postTag = '\n- ' + _.uniq(postTag).join('\n- ');
-            if (categories.length) categories = '\n- ' + _.uniq(categories).join('\n- ');
-
-            var content = [
-              'title: "' + postTitle.replace(/"/g, '\\"') + '"',
-              'id: ' + id,
-              'date: ' + postDate,
-              'tags: ' + (postTag ? postTag : ''),
-              'categories: ' + (categories || 'uncategory'),
-              '---'
-            ];
-
-            file.write(sourceDir + postStatus + decodeURIComponent(postLink) + '.md', content.join('\n') + '\n\n' + postContent, next);
-            break;
-
-          case 'page':
-            length++;
-
-            var content = [
-              'title: ' + postTitle,
-              'date: ' + postDate,
-              '---'
-            ];
-
-            file.write(sourceDir + postLink + '/index.md', content.join('\n') + '\n\n' + postContent, next);
-            break;
-
-          default:
-            next();
-        }
+        log.i('%s found: %s', captialize(type), title);
+        post.create(data, next);
       }, function(err){
-        if (err) throw err;
-        next(null, length);
+        if (err) return next(err);
+
+        log.i('%d posts migrated.', count);
       });
     }
-  ], function(err, length){
-    if (err) throw err;
-    console.log('%d posts migrated.', length);
-  });
+  ], callback);
 });
