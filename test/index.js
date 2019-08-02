2f69513a241dacf5a4a75a0a6a3bf2a2ca68cbbc
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-02 18:20:01 +0930

    test: initial commit

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const should = require('chai').should(); // eslint-disable-line
+
+describe('hexo-migrator-wordpress', () => {
+  // Tests.
+  it('should migrate posts.', () => {
+    return true;
+  });
+});
