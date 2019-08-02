891d0a749f7e00603b6d5fcbafb796093f8cd477
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-02 18:29:25 +0930

    style: eslint

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var xml2js = require('xml2js'),
-  async = require('async'),
-  TurndownService = require('turndown'),
-  request = require('request'),
-  file = require('fs');
+'use strict';
+
+/* global hexo */
+
+var xml2js = require('xml2js');
+var async = require('async');
+var TurndownService = require('turndown');
+var request = require('request');
+var file = require('fs');
 
 var turndownService = new TurndownService();
 
-var captialize = function(str){
+var captialize = function(str) {
   return str[0].toUpperCase() + str.substring(1);
 };
-function replaceTwoBrace(str){
-    str = str.replace(/{{/g, '{ {');
-    return str;
-};
-hexo.extend.migrator.register('wordpress', function(args, callback){
+function replaceTwoBrace(str) {
+  str = str.replace(/{{/g, '{ {');
+  return str;
+}
+hexo.extend.migrator.register('wordpress', function(args, callback) {
   var source = args._.shift();
 
-  if (!source){
+  if (!source) {
     var help = [
       'Usage: hexo migrate wordpress <source>',
       '',
@@ -27,44 +31,44 @@ hexo.extend.migrator.register('wordpress', function(args, callback){
     return callback();
   }
 
-  var log = hexo.log,
-    post = hexo.post;
+  var log = hexo.log;
+  var post = hexo.post;
 
   log.i('Analyzing %s...', source);
 
   async.waterfall([
-    function(next){
+    function(next) {
       // URL regular expression from: http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
-      if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/)){
-        request(source, function(err, res, body){
+      if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)/)) {
+        request(source, function(err, res, body) {
           if (err) throw err;
-          if (res.statusCode == 200) next(null, body);
+          if (res.statusCode === 200) next(null, body);
         });
       } else {
         file.readFile(source, next);
       }
     },
-    function(content, next){
+    function(content, next) {
       xml2js.parseString(content, next);
     },
-    function(xml, next){
+    function(xml, next) {
       var count = 0;
 
-      async.each(xml.rss.channel[0].item, function(item, next){
-        if (!item['wp:post_type']){
+      async.each(xml.rss.channel[0].item, function(item, next) {
+        if (!item['wp:post_type']) {
           return next();
         }
 
-        var title = item.title[0].replace(/"/g, "\\\""),
-          id = item['wp:post_id'][0],
-          date = item['wp:post_date'][0],
-          slug = item['wp:post_name'][0],
-          content = item['content:encoded'][0],
-          comment = item['wp:comment_status'][0],
-          status = item['wp:status'][0],
-          type = item['wp:post_type'][0],
-          categories = [],
-          tags = [];
+        var title = item.title[0].replace(/"/g, '\\"');
+        var id = item['wp:post_id'][0];
+        var date = item['wp:post_date'][0];
+        var slug = item['wp:post_name'][0];
+        var content = item['content:encoded'][0];
+        var comment = item['wp:comment_status'][0];
+        var status = item['wp:status'][0];
+        var type = item['wp:post_type'][0];
+        var categories = [];
+        var tags = [];
 
         if (!title && !slug) return next();
         if (type !== 'post' && type !== 'page') return next();
@@ -73,11 +77,11 @@ hexo.extend.migrator.register('wordpress', function(args, callback){
         content = turndownService.turndown(content).replace(/\r\n/g, '\n');
         count++;
 
-        if (item.category){
-          item.category.forEach(function(category, next){
+        if (item.category) {
+          item.category.forEach(function(category, next) {
             var name = category._;
 
-            switch (category.$.domain){
+            switch (category.$.domain) {
               case 'category':
                 categories.push(name);
                 break;
@@ -91,11 +95,11 @@ hexo.extend.migrator.register('wordpress', function(args, callback){
 
         var data = {
           title: title || slug,
-          url: +id+".html",
+          url: +id + '.html',
           id: +id,
           date: date,
           content: content,
-          layout: status === 'draft' ? 'draft' : 'post',
+          layout: status === 'draft' ? 'draft' : 'post'
         };
 
         if (type === 'page') data.layout = 'page';
@@ -106,7 +110,7 @@ hexo.extend.migrator.register('wordpress', function(args, callback){
 
         log.i('%s found: %s', captialize(type), title);
         post.create(data, next);
-      }, function(err){
+      }, function(err) {
         if (err) return next(err);
 
         log.i('%d posts migrated.', count);
