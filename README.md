4af8c349ecba8cbc5744392b2bf20f0ebe4c244b
curbengh <43627182+curbengh@users.noreply.github.com>
2020-01-12 03:01:05 +0000

    feat: limit, alias options. native async

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Migrate your blog from WordPress to [Hexo].
 
 ## Install
 
-In your blog folder, add this npm dependencie to your project :
- 
+In your blog folder, add this npm dependencie to your project:
+
 ``` bash
 $ npm install hexo-migrator-wordpress --save
 ```
@@ -20,7 +20,19 @@ Export your WordPress in "Tools" → "Export" → "WordPress" in your dashboard.
 Execute the following command after installed. `source` is the file path or URL of WordPress export file.
 
 ``` bash
-$ hexo migrate wordpress <source>
+$ hexo migrate wordpress <source> [--options]
 ```
 
-[Hexo]: http://zespia.tw/hexo
+- **alias**: Populates the `alias` setting in the front-matter, for use with the [hexo-generator-alias](http://github.com/hexojs/hexo-generator-alias) module. This is useful for generating redirects.
+- **limit**: Maximum number of posts to import from the input file. All posts are imported by default.
+  * Example:
+  ``` bash
+  $ hexo migrate wordpress /path/export.xml --limit 3
+  ```
+  * This doesn't apply to pages, all pages will be imported.
+- **skipduplicate**: Skip posts with similar title as existing ones.
+  * If the input contains a post titled 'Foo Bar' and there is an existing post named 'Foo-Bar.md', then that post will not be migrated.
+  * The comparison is case-insensitive; a post titled 'FOO BAR' will be skipped if 'foo-bar.md' exists.
+  * Without this option (default), this plugin will continue to migrate that post and create a post named 'Foo-Bar-1.md'
+
+[Hexo]: http://hexo.io/
--- test/index.js
+++ test/index.js
@@ -1,10 +1,151 @@
 'use strict';
 
-const should = require('chai').should(); // eslint-disable-line
+require('chai').should();
+const should = require('chai').should();
+const { join } = require('path');
+const { exists, listDir, readFile, rmdir, writeFile } = require('hexo-fs');
+const Hexo = require('hexo');
+const hexo = new Hexo(process.cwd(), { silent: true });
+const m = require('../lib/migrator.js').bind(hexo);
+const parseFeed = require('../lib/feed');
+const { spy } = require('sinon');
+const log = spy(hexo.log);
 
-describe('hexo-migrator-wordpress', () => {
-  // Tests.
-  it('should migrate posts.', () => {
-    return true;
+const filterPost = ({ type }) => {
+  return type === 'post';
+};
+
+describe('migrator', function() {
+  this.timeout(5000);
+
+  before(() => hexo.init());
+
+  afterEach(async () => {
+    const exist = await exists(hexo.source_dir);
+    if (exist) await rmdir(hexo.source_dir);
+  });
+
+  it('default - file', async () => {
+    await m({ _: [join(__dirname, 'fixtures/wordpress.xml')] });
+    const exist = await exists(join(hexo.source_dir, '_posts', 'hello-world.md'));
+
+    exist.should.eql(true);
+  });
+
+  it('default - logging', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    await m({ _: [path] });
+
+    const input = await readFile(path);
+    const { items } = await parseFeed(input);
+
+    const { firstCall, lastCall } = log.i;
+
+    firstCall.calledWith('Analyzing %s...', path).should.eql(true);
+    lastCall.calledWith('%d posts migrated.', items.length).should.eql(true);
+  });
+
+  it('default - logging (untitled post)', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    await m({ _: [path] });
+
+    log.w.calledWith('%d posts did not have titles and were prefixed with "Untitled Post".', 1).should.eql(true);
+  });
+
+  it.skip('default - url', async () => {
+    await m({ _: ['https://github.com/hexojs/hexo-migrator-wordpress/raw/master/test/fixtures/wordpress.xml'] });
+    const exist = await exists(join(hexo.source_dir, '_posts', 'dove-comprare-200-mg-celebrex.md'));
+
+    exist.should.eql(true);
+  });
+
+  it('no argument', async () => {
+    try {
+      await m({ _: [''] });
+      should.fail();
+    } catch (err) {
+      err.message.split('\n')[0].should.eql('Usage: hexo migrate wordpress <source> [--options]');
+    }
+  });
+
+  it('invalid url', async () => {
+    const url = 'http://does.not.exist/';
+    try {
+      await m({ _: [url] });
+      should.fail();
+    } catch (err) {
+      err.message.includes('RequestError:').should.eql(true);
+    }
+  });
+
+  it('invalid path', async () => {
+    const path = 'does/not/exist';
+    try {
+      await m({ _: [path] });
+      should.fail();
+    } catch (err) {
+      err.message.includes('Error: ENOENT: no such file or directory').should.eql(true);
+    }
+  });
+
+  it('option - limit', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    const limit = 2;
+    await m({ _: [path], limit });
+    const posts = await listDir(join(hexo.source_dir, '_posts'));
+    posts.length.should.eql(limit);
+  });
+
+  it('option - invalid limit', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    const limit = 9000;
+    await m({ _: [path], limit });
+    const posts = await listDir(hexo.source_dir);
+    const input = await readFile(path);
+    const expected = await parseFeed(input);
+
+    posts.length.should.eql(expected.items.length);
+  });
+
+  it('option - skipduplicate', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    const postDir = join(hexo.source_dir, '_posts');
+    await writeFile(join(postDir, 'hello-world.md'), 'foo');
+    await m({ _: [path], skipduplicate: true });
+
+    const posts = await listDir(postDir);
+    const input = await readFile(path);
+    const expected = await parseFeed(input);
+    const expectedPosts = expected.items.filter(filterPost);
+
+    posts.length.should.eql(expectedPosts.length);
+  });
+
+  it('option - skipduplicate (no existing post)', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    const postDir = join(hexo.source_dir, '_posts');
+    await m({ _: [path], skipduplicate: true });
+
+    const posts = await listDir(postDir);
+    const input = await readFile(path);
+    const expected = await parseFeed(input);
+    const expectedPosts = expected.items.filter(filterPost);
+
+    posts.length.should.eql(expectedPosts.length);
+  });
+
+  it('option - skipduplicate disabled', async () => {
+    const path = join(__dirname, 'fixtures/wordpress.xml');
+    const postDir = join(hexo.source_dir, '_posts');
+    await writeFile(join(postDir, 'hello-world.md'), 'foo');
+    await m({ _: [path] });
+
+    const posts = await listDir(postDir);
+    const input = await readFile(path);
+    const expected = await parseFeed(input);
+    const expectedPosts = expected.items.filter(filterPost);
+
+    posts.length.should.not.eql(expectedPosts.length);
+    (posts.length > expectedPosts.length).should.eql(true);
   });
 });
