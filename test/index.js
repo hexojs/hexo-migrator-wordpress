866bf3b89eadb64dde896acf790028fe9f4715dd
MDLeom <43627182+curbengh@users.noreply.github.com>
2020-09-29 02:48:10 +0000

    test: use .invalid for testing
    - RFC2606

--- test/index.js
+++ test/index.js
@@ -692,7 +692,7 @@ describe('migrator', function() {
     });
 
     it('invalid image link', async () => {
-      const imageUrl = 'http://does.not.exist/image.jpeg';
+      const imageUrl = 'http://foo.invalid/image.jpeg';
       const xml = wp(imageUrl, 'image.png');
       const path = join(__dirname, 'image.xml');
       await writeFile(path, xml);
@@ -729,7 +729,7 @@ describe('migrator', function() {
   });
 
   it('invalid url', async () => {
-    const url = 'http://does.not.exist/';
+    const url = 'http://foo.invalid/';
     try {
       await m({ _: [url] });
       should.fail();
